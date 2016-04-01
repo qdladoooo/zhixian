ecfa395a4faafd1df0891b30a9c06ce1638d753b
Dlad <qdladoooo@gmail.com>
2016-04-01 20:14:14 +0800

    laravel5.1 project zhixian

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UserController extends Controller
+{
+    public function getIndex() {
+        return view('index');
+    }
+
+    public function getLogin() {
+        return "hello world?";
+    }
+
+    public function getLogout() {
+        return "hello world?";
+    }
+
+
+
+}
